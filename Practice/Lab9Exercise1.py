a010ac747bcfddb3a9d58b66e556253f98e1e29b
Jin Shim <jinenstar@gmail.com>
2026-02-08 15:54:38 -0800

    Delete Practice directory

--- Practice/Lab9Exercise1.py
+++
@@ -1,7 +0,0 @@
-# 1. match TATA or nothing
-# 2. match TATAA followed by A or T  (such motifs are called TATA boxes)
-# 3. match 3-mer consisting of the dimer CG that is not immediately followed by C or G
-
-# 1. r'TATA'
-# 2. TATAA[A|T]
-# 3. CG[^C|G]
